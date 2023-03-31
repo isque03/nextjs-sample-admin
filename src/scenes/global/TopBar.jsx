c0bd094325d309dd8f84f2f50904a47f385241d7
Adam Belmont <abelmont03@gmail.com>
2023-03-30 20:55:40 -0400

    checkpoint

--- src/scenes/global/TopBar.jsx
+++ src/scenes/global/TopBar.jsx
@@ -1,5 +1,27 @@
+import { Box, IconButton, useTheme } from '@mui/material';
+import { useContext } from 'react';
+import { ColorModeContext, tokens } from '../../theme';
+import InputBase from '@mui/material';
+import LightModeOutlined from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlined from '@mui/icons-material/DarkModeOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+
+
 const Topbar = () => {
-    return <div>Topbar</div>
+    // grab the color mode from the context
+    const theme = useTheme();
+    // grab the color mode from the context for the current theme (light or dark)
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+    return <Box display="flex" justifyContent={"space-between"} alignItems={"center"} p={2}>
+        {/* Search bar */}
+        <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px"></Box>
+        <IconButton></IconButton>
+    </Box>;
+    
 }
- 
+
 export default Topbar;
